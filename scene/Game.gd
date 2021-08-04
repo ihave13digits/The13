34b110fa4cea52c028b81a4cd78588c518fc8d75
digits <ihave13digits@protonmail.com>
2021-08-03 23:11:03 -0400

    Refined bobbing animation.  Added flashlight swinging, HUD, Raycast.  Worm grabs player after 100 steps.

--- scene/Game.gd
+++ scene/Game.gd
@@ -1,15 +1,26 @@
 extends Node
 
-var player
+onready var hud
+onready var cursor
+onready var player
 
 func _ready():
-	player = load(Data.mob['player']).instance()
+	hud = $HUD
+	cursor = $HUD/Center/Cursor
+	player = Data.mob['player'].instance()
 	add_child(player)
+	player.connect('update_cursor', self, 'update_cursor')
 	player.connect('triggered_event', self, 'spawn')
+
+func update_cursor():
+	if player.cursor.get_collider() != null:
+		cursor.modulate = Color(1.0, 1.0, 1.0, 0.5)
+	else:
+		cursor.modulate = Color(1.0, 1.0, 1.0, 0.125)
 
 func spawn(m='worm'):
 	if Data.trigger_worm == false:
 		Data.trigger_worm = true
-		var i = load(Data.mob[m]).instance()
+		var i = Data.mob[m].instance()
 		i.translation = Vector3(player.translation.x, player.translation.y, player.translation.z)
 		add_child(i)
