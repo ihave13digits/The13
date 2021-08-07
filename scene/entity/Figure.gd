dff0692a712daaf6f169437a87de7340e9f34426
digits <ihave13digits@protonmail.com>
2021-08-06 23:33:41 -0400

    updated figure mechanics

--- scene/entity/Figure.gd
+++ scene/entity/Figure.gd
@@ -1,8 +1,13 @@
 extends Spatial
 
+onready var hitbox = $Box/Shape
+
 func _process(_delta):
 	look_at(get_parent().player.translation, Vector3.UP)
 	rotate_object_local(Vector3(0, 1, 0), 3.14159)
+
+func hide_from_player():
+	$AnimationPlayer.play("crouch")
 
 func _on_AnimationPlayer_animation_finished(_anim_name):
 	queue_free()
